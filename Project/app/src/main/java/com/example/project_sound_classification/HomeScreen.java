ba59e98a0b8065a45c2077b2c91b99b8f4ec3f33
SonYoungGin <younggin446@naver.com>
2023-03-21 00:19:27 +0900

    feature: 툴바 구현

---
+++ Project/app/src/main/java/com/example/project_sound_classification/HomeScreen.java
@@ -0,0 +1,33 @@
+package com.example.project_sound_classification;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.project_sound_classification.databinding.HomeScreenBinding;
+
+public class HomeScreen extends Fragment {
+
+    private HomeScreenBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = HomeScreenBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
