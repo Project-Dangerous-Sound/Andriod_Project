065a74dd0dceb3b7d639524566c12a95e42214dd
Kim seong won <worni5015@gmail.com>
2023-03-24 16:15:30 +0900

    Priority 페이지 추가 완료

---
+++ Project/app/src/main/java/com/example/project_sound_classification/Person.java
@@ -0,0 +1,34 @@
+package com.example.project_sound_classification;
+
+public class Person {
+    int image;
+    String name;
+    int priority;
+
+    public Person(int image, String name, int priority){
+        this.image = image;
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public int getImage() {
+        return image;
+    }
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
--- Project/app/src/main/java/com/example/project_sound_classification/PriorityActivity.java
+++ Project/app/src/main/java/com/example/project_sound_classification/PriorityActivity.java
@@ -1,21 +1,41 @@
 package com.example.project_sound_classification;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PriorityActivity extends AppCompatActivity {
-
+    static RecyclerView rv;
+    ListAdapter adapter;
+    ItemTouchHelper helper;
+    static Singleton singleton;
+    static public void reset(){
+        rv.setAdapter(singleton.adapter);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_priority);
+        setContentView(R.layout.sound_list);
+        singleton = new Singleton();
+        rv = findViewById(R.id.rv);
+        //RecyclerView의 레이아웃 방식을 지정
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        rv.setLayoutManager(manager);
+
+        //RecyclerView의 Adapter 세팅
+        adapter = new ListAdapter();
+        rv.setAdapter(singleton.adapter);
+
+        //ItemTouchHelper 생성
+        helper = new ItemTouchHelper(singleton.itemTouchHelperCallback);
+        //RecyclerView에 ItemTouchHelper 붙이기
+        helper.attachToRecyclerView(rv);
 
 
+        //Adapter에 데이터 추가
+        //SingleTon.jave에서 실행
     }
-
 }
