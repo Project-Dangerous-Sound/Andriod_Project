441838db7f96cebb0a35712176d71f98b4428df0
kimmoonwoong <ansdnd1500@naver.com>
2023-04-02 20:01:55 +0900

    commit

--- Project/app/src/main/java/com/example/project_sound_classification/ItemTouchHelperListener.java
+++ Project/app/src/main/java/com/example/project_sound_classification/ItemTouchHelperListener.java
@@ -1,7 +1,11 @@
 package com.example.project_sound_classification;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+
 public interface ItemTouchHelperListener {
-    boolean onItemMove(int from_position, int to_position);
+    boolean onItemMove(int from_position, int to_position) throws JSONException, IOException;
     void onItemSwipe(int position);
 
 }
